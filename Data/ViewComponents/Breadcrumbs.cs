e35d372b3ad696577f1841c01a786f4c3f26c1bb
Wiktoria Solska <wiksolska@gmail.com>
2023-08-30 10:15:57 +0200

    code refactoring in Breadcrumb class

--- Data/ViewComponents/Breadcrumbs.cs
+++ Data/ViewComponents/Breadcrumbs.cs
@@ -4,13 +4,6 @@ namespace MaturaToBzdura.Data.ViewComponents
 {
     public class Breadcrumbs : ViewComponent
     {
-        private readonly AppDbContext _context;
-
-        public Breadcrumbs(AppDbContext context)
-        {
-            _context = context;
-        }
-
         public IViewComponentResult Invoke()
         {
             return View();
